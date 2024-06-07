d6b21e5ce4163521255e0f3bce3ef6949efb357a
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-06-07 17:08:30 -0400

    Provide functions to manipulate deck

--- src/app/library/lib.ts
+++ src/app/library/lib.ts
@@ -40,3 +40,57 @@ export function getFaceValue(card: Card): FaceValue{
     return card >> SUIT_BIN_WIDTH;
 }
 
+export type Cards = Card[];
+export type Deck = Cards;
+export type Pile = Cards;
+
+export function createSuits(): Suit[]{
+    return [Suit.Diamonds, Suit.Clubs, Suit.Hearts, Suit.Spades];
+}
+
+export function createFaceValues(): FaceValue[]{
+    return [
+        FaceValue.Two,
+        FaceValue.Three,
+        FaceValue.Four,
+        FaceValue.Five,
+        FaceValue.Six,
+        FaceValue.Seven,
+        FaceValue.Eight,
+        FaceValue.Nine,
+        FaceValue.Ten,
+        FaceValue.Jack,
+        FaceValue.Queen,
+        FaceValue.King,
+        FaceValue.Ace
+    ]
+}
+
+
+
+export function createDeck(): Deck{
+    let deck : Deck = createSuits().flatMap(s => createFaceValues().flatMap(f => createCard(s,f)));
+    shuffle(deck);
+    return deck; // just a way to create an array of cards using nested flatmap to simulate nested loops
+}
+
+export function dealCards(deck: Deck,numberOfCards: number): Pile{
+    return deck.splice(0,numberOfCards);
+}
+
+function shuffle(deck: Deck): void {
+    let currentIndex = deck.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [deck[currentIndex], deck[randomIndex]] = [
+        deck[randomIndex], deck[currentIndex]];
+    }
+  }
+  
