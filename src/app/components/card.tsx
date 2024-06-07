501a150a37f2ce9d0e0551bcbae8e4338edb471d
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-06-07 12:17:13 -0400

    Create basic components

---
+++ src/app/components/card.tsx
@@ -0,0 +1,10 @@
+import {Card as TCard, getSuit, getFaceValue} from '../library/lib';
+
+interface CardProp{
+    card: TCard,
+    isFlipped: boolean
+}
+
+export default function Card({card: TCard}: CardProp){
+
+}
