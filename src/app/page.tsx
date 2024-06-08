729574dc0cfd440171fa6abc17e1d57736bb54c8
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-06-08 18:07:00 -0400

    Set up hand, pile, and deck states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,67 @@
-import Image from "next/image";
+"use client"
+
+import {useEffect, useState} from 'react'
+import Hand from "./components/hand";
+import Pile from './components/pile';
+import {Deck, createDeck, dealCards, Pile as TPile, Card, Destination, Player} from './library/lib';
+import { Box } from "@chakra-ui/react"
+
 
 export default function Home() {
+  const [deck, setDeck] = useState<Deck>([]);
+  const [P1hand, setP1hand] = useState<TPile>([]);
+  const [P2hand, setP2hand] = useState<TPile>([]);
+  const [P1DrawPile, setP1DrawPile] = useState<TPile>([]);
+  const [P2DrawPile, setP2DrawPile] = useState<TPile>([]);
+  const [centerPile1, setcenterPile1] = useState<TPile>([]);
+  const [centerPile2, setcenterPile2] = useState<TPile>([]);
+  const [centerDrawPile1, setcenterDrawPile1] = useState<TPile>([]);
+  const [centerDrawPile2, setcenterDrawPile2] = useState<TPile>([]);
+
+  function useCard(card : Card, player : Player){
+    if(player == Player.Player1){
+      let index : number = P1hand.indexOf(card);
+      let targetCard : Card = P1hand[index];
+      setP1hand(P1hand.filter(c => c != targetCard));
+      setcenterPile1([targetCard, ...centerDrawPile1])
+    }else{
+      let index : number = P2hand.indexOf(card);
+      let targetCard : Card = P2hand[index];
+      setP2hand(P2hand.filter(c => c != targetCard));
+      setcenterPile1([targetCard, ...centerDrawPile1])
+    }
+  }
+
+  useEffect(() => {
+    let temp_deck : Deck = createDeck();
+    setP1hand(dealCards(temp_deck, 4));
+    setP2hand(dealCards(temp_deck,4));
+    setP1DrawPile(dealCards(temp_deck, 16));
+    setP2DrawPile(dealCards(temp_deck, 16));
+    setcenterPile1(dealCards(temp_deck, 1));
+    setcenterPile2(dealCards(temp_deck, 1));
+    setcenterDrawPile1(dealCards(temp_deck, 5));
+    setcenterDrawPile2(dealCards(temp_deck, 5));
+    setDeck(temp_deck);
+  }, [])
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Box display="flex" alignItems="center" justifyContent="space-between" flexDirection="column" backgroundColor="#35654D" position="fixed" height="100%" width="100%">
+        <Box display="flex" alignItems="center" justifyContent="space-between" margin="30px">
+          <Hand cards={P1hand} useCard={useCard} player = {Player.Player1}></Hand>
+          <Pile Cards = {P1DrawPile} isFlipped={true}/>
+        </Box>
+        <Box display="flex" alignItems="center" justifyContent="space-between" margin="30px" id="center">
+          <Pile Cards = {centerDrawPile1} isFlipped={true}/>
+          <Pile Cards = {centerPile1} isFlipped={false}/>
+          <Pile Cards = {centerPile2} isFlipped={false}/>
+          <Pile Cards = {centerDrawPile2} isFlipped={true}/>
+        </Box>
+        <Box display="flex" alignItems="center" justifyContent="space-between" margin="30px">
+          <Hand cards={P2hand} useCard={useCard} player= {Player.Player2}></Hand>
+          <Pile Cards = {P2DrawPile} isFlipped={true}/>
+        </Box>
+    </Box>
   );
 }
