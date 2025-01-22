4ec002ffddc4181cd7572cd1b5db997487d904ea
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-01-22 16:23:47 -0500

    Update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript",{
-    "typescript-eslint/no-empty-object-type": "off"
+    "@typescript-eslint/no-empty-object-type": "off"
   }),
   
 ];
