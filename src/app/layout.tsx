7166472f7951de864a323bbef7b78f93b4927a4e
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-01-22 16:48:20 -0500

    Add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
